e5148cade6e5abd92534158044b043fccdc3c7a4
user <wodndhksk@gmail.com>
2022-07-11 10:58:29 +0900

    cli 프로젝트 설명 및 생성 실습

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <app-header v-bind:propsdata="str"
+    v-on:renew="renewStr"
+    ></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+export default {
+  data(){
+    return{
+      str: 'Header1'
+    }
+  },
+  components: {
+    'app-header' : AppHeader
+  },
+  methods: {
+    renewStr(){
+      this.str = 'hi';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/a.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>header</div>
+</template>
+
+<script>
+export default {
+    methods: {
+        addNum() {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
